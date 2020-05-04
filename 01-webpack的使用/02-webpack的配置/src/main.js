43f794e9f95029048f15260dbf72ec0e422f5136
qjc <775320358@qq.com>
2020-05-04 12:44:43 +0800

    webpack使用

--- 01-webpack的使用/02-webpack的配置/src/main.js
+++ 01-webpack的使用/02-webpack的配置/src/main.js
@@ -1,9 +1,9 @@
 //1.使用common.js的模块化规范
-const {add,mul} = require('./mathUtil.js')
+const {add,mul} = require('./js/mathUtil.js')
 console.log(add(20,30));
 console.log(mul(20,30));
 
 //2.使用ES6的模块化规范
-import {name,age} from "./info.js"
+import {name,age} from "./js/info.js"
 console.log(name);
 console.log(age);
