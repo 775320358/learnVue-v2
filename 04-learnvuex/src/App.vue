fbb647ebfe309da71c7d4c4aad84a7eb211bde2e
qjc <775320358@qq.com>
2020-05-07 16:55:35 +0800

    vuex初体验

---
+++ 04-learnvuex/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <h2>-----App组件----</h2>
+    <h2>{{$store.state.counter}}</h2>
+    <button @click="$store.state.counter++">+</button>
+    <button @click="$store.state.counter--">-</button>
+    <h2>---Hello---</h2>
+    <hello-vuex></hello-vuex>
+  </div>
+</template>
+
+<script>
+import HelloVuex from './components/HelloVuex'
+export default {
+  name: 'App',
+  components: {
+    HelloVuex
+  },
+  data(){
+    return{
+      message:'我是App组件',
+    }
+  },
+}
+</script>
+
+<style>
+</style>
---
+++ 04-learnvuex/src/components/HelloVuex.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h2>{{$store.state.counter}}</h2>
+  </div>
+</template>
+<script>
+export default {
+  name: 'HelloVuex',
+  data() {
+    return {}
+  },
+  methods: {}
+}
+</script>
+<style scoped>
+</style>
